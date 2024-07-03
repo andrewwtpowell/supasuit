da7c17e7cf137c26b1db606638526c3dd27e7536
andrewwtpowell <powell.andrew26@gmail.com>
2024-07-02 20:40:01 -0600

    incorporated all dev dependencies

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,12 +2,24 @@
 
 set -xe
 
+# Copy config files to VM
+set USER_HOME=/home/vagrant
+cp /vagrant/.tmux.conf $USER_HOME/.tmux.conf
+cp /vagrant/.bashrc $USER_HOME/.bashrc
+cp -r /vagrant/nvim $USER_HOME/.config/
+
+# Source configs
+source $USER_HOME/.bashrc
+
 # Install necessary packages
 sudo apt-get update
 
+# Dev Tools
+sudo apt-get install -y build-essential cmake autoconf libtool pkg-config git curl
+
 # Docker
 # Add Docker's official GPG key:
-sudo apt-get install ca-certificates curl
+sudo apt-get install -y ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -18,18 +30,24 @@ echo \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # Create docker group
-sudo groupadd docker
+sudo groupadd docker || true
 sudo usermod -aG docker $USER
 newgrp docker
 
 # Go
-
-# gRPC
+sudo apt-get install -y golang-go
 
 # Neovim
+sudo apt-get install -y gettext
+git clone https://github.com/neovim/neovim
+cd neovim
+git checkout stable
+make CMAKE_BUILD_TYPE=Release
+make install
+sudo apt-get install -y ripgrep
 
-# Copy config files to VM
-
+# Tmux
+sudo apt-get install -y tmux
